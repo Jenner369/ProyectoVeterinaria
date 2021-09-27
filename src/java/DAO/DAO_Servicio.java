16741d0fa821da60db60844775b2f52ec6057b4c
USER <USER@DESKTOP-FV4RS79>
2021-09-26 20:46:34 -0500

    DAOServicio

--- src/java/DAO/DAO_Servicio.java
+++ src/java/DAO/DAO_Servicio.java
@@ -1,5 +1,23 @@
 package DAO;
 
+import Beans.Beans_Mascota;
+import Config.cnx;
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DAO_Servicio {
+    cnx cn;
+    Connection con;
+    Beans_Mascota b;
+    CallableStatement cs;
+    private ResultSet rs;
     
+    public DAO_Servicio() throws SQLException, IOException {
+        this.cn = new cnx();
+    }
 }
