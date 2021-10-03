d93c2caf2f505b071f51b625adba002946da4da5
USER <USER@DESKTOP-FV4RS79>
2021-10-03 18:22:10 -0500

    as

--- web/Utiles/Js/loadMenu.js
+++ web/Utiles/Js/loadMenu.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function loadNavBarMenu() {
     $('nav').load('navbarMenu.jsp')
 }
 
 $(document).ready( function() {
     loadNavBarMenu()
-=======
-function loadNavBarMenu() {
-    $('nav').load('navbarMenu.jsp')
-}
-
-$(document).ready( function() {
-    loadNavBarMenu()
->>>>>>> f57620da6c94f5f029ca702778b86ada002ce3f7
 })
