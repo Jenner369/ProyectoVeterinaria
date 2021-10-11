1e483827a6c95e96ba8084a5b80074425439a8f3
USER <USER@DESKTOP-FV4RS79>
2021-10-11 01:47:56 -0500

    Mascota

--- src/java/Controlador/Servlet_Mascota.java
+++ src/java/Controlador/Servlet_Mascota.java
@@ -1,22 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
 
+import DAO.DAO_Mascota;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author jedur
- */
 @WebServlet(name = "Servlet_Mascota", urlPatterns = {"/Servlet_Mascota"})
 public class Servlet_Mascota extends HttpServlet {
 
@@ -58,7 +54,7 @@ public class Servlet_Mascota extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
     /**
@@ -72,7 +68,20 @@ public class Servlet_Mascota extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String Nombre = request.getParameter("Nombre");
+        String Raza = request.getParameter("Raza");
+        String Sexo = request.getParameter("Sexo");
+        String Tipo = request.getParameter("Tipo");
+        int ID = Integer.parseInt(request.getParameter("ID"));
+        
+        try {
+            DAO_Mascota OperacionMascota = new DAO_Mascota();
+            OperacionMascota.RegistrarMascota(Nombre, Raza, Sexo, Tipo, "a", ID);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Servlet_Mascota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
     /**
